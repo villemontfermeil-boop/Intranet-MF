d028487745073b385570cbd394dfbd0254f6e48e
Dev montfermeil <ville.montfermeil@gmail.com>
2025-12-17 07:23:23 -0800

    commit

--- IntranetFront/my-montfermeil/app/component/Header.tsx
+++ IntranetFront/my-montfermeil/app/component/Header.tsx
@@ -1,46 +1,16 @@
 'use client';
 import { useState, useEffect, use } from 'react';
 import './stylheader.css';
+import { useRouter } from 'next/navigation';
 function Header({ isConnectd }: { isConnectd?: boolean }) {
     const [clicked, setClicked] = useState(false);
+    const [clickedOUT, setClickedOUT] = useState(false);
     const [clientConnected, setClientConnected] = useState(false);
-    useEffect(() => {
-        if (clicked) {
-            (async () => {
-                const result = await handleLogin();
-                setClientConnected(result);
-            })();
-            setClicked(false);
-        }
-        if (clientConnected) {
-         isConnectd = true;
-        }else{
-         isConnectd = false;
-        }
+    const router = useRouter();
 
-    }, [clicked]);
-
-    return (
-        <div className="Thebody">
-            <header className="login-header">
-                {/* <img src="app/component/logo.png" /> */}
-                <h1>Welcome Back!</h1>
-                <form className="login-form">{isConnectd == true ?
-                    <button type="button" >Logout</button> : <>
-                        <input type="text" placeholder="Email" id='Email' name="email" required />
-                        <input type="password" placeholder="Password" id='Pass' name="password" required />
-                        <button type="button" onClick={() => setClicked(true)}>Login</button>
-                        <button type="button"  >Sign Up</button> </>}
-                </form>
-            </header>
-        </div>
-    );
-}
-
-async function handleLogin() {
+    async function handleLogin() {
     const email = document.getElementById('Email') as HTMLInputElement;
     const password = document.getElementById('Pass') as HTMLInputElement;
-    const router = useRouter();
     
     if (!email || !password) {
         console.error('Email or Password input not found');
@@ -59,6 +29,16 @@ async function handleLogin() {
     }).then((response) => response.json())
         .then((data) => {
             console.log('Success:', data);
+            localStorage.setItem('id', data.id);
+            localStorage.setItem('nom', data.nom);
+            localStorage.setItem('prenom', data.prenom);
+            localStorage.setItem('mail', data.mail);
+            localStorage.setItem('isAdmin', data.isAdmin);
+            localStorage.setItem('isConnected', data.isConnected);
+            localStorage.setItem('numero', data.numero);
+            
+
+            router.refresh();
             return true;
         })
         .catch((error) => {
@@ -67,4 +47,66 @@ async function handleLogin() {
         });
     return data;
 }
+    async function handleLogout() {
+
+        var data = await fetch('http://localhost:8080/salaries/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer intranetMF-token'
+            },
+            body: new URLSearchParams({
+                mail: localStorage.getItem('mail') || ''
+            })
+        }).then((response) => response.json())
+
+        if (!data.error) {
+            console.log('Logout Success:', data);
+             localStorage.removeItem('salarieData');
+             localStorage.clear();
+            router.refresh();
+            isConnectd = false;
+        }else {
+            console.error('Logout Error:', data.error);
+        }
+       
+    }
+    useEffect(() => {
+        if (clickedOUT) {
+            handleLogout();
+            setClickedOUT(false);
+        }
+        if (clicked) {
+            (async () => {
+                const result = await handleLogin();
+                setClientConnected(result);
+            })();
+            setClicked(false);
+        }
+        if (clientConnected) {
+         isConnectd = true;
+        }else{
+         isConnectd = false;
+        }
+
+    }, [clicked, clickedOUT]);
+
+    return (
+        <div className="Thebody">
+            <header className="login-header">
+                {/* <img src="app/component/logo.png" /> */}
+                <h1>Welcome Back!</h1>
+                <form className="login-form">{isConnectd == true ?
+                    <button type="button" onClick={() => setClickedOUT(true)} >Logout</button> : <>
+                        <input type="text" placeholder="Email" id='Email' name="email" required />
+                        <input type="password" placeholder="Password" id='Pass' name="password" required />
+                        <button type="button" onClick={() => setClicked(true)}>Login</button>
+                        <button type="button"  >Sign Up</button> </>}
+                </form>
+            </header>
+        </div>
+    );
+}
+
+
 export default Header;
--- IntranetFront/my-montfermeil/app/layout.tsx
+++ IntranetFront/my-montfermeil/app/layout.tsx
@@ -1,12 +1,12 @@
 'use client';
 import Header from "./component/Header";
 
-
+ var localStorage = globalThis?.localStorage;
 function RootLayout({ children } : { children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>
-        <Header isConnectd={false}/>
+        <Header isConnectd={localStorage.length > 0 ? true : false}/>
         {children}
       </body>
     </html>
