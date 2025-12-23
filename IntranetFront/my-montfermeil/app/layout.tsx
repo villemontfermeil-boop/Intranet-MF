c0a33e17a2e4e799537db1c0a0a46a506fa9e6d1
Dev montfermeil <ville.montfermeil@gmail.com>
2025-12-23 11:31:20 +0100

    Commit 23.12.2025

--- IntranetFront/my-montfermeil/app/layout.tsx
+++ IntranetFront/my-montfermeil/app/layout.tsx
@@ -1,28 +1,25 @@
 'use client';
+
 import Header from "./component/Header";
 import Footer from "./component/footer";
- var localStorage = globalThis?.localStorage;
-function RootLayout({ children } : { children: React.ReactNode}) {
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+  const prenom = sessionStorage.getItem('prenom');
+  const nom = sessionStorage.getItem('nom');
+
   return (
     <html lang="en">
       <body>
         <div>
-        <Header nom={localStorage.length > 0 ? localStorage.getItem('prenom')+ ' '+ localStorage.getItem('nom'): null}/>
-          
+          <Header nom={prenom && nom ? prenom + ' ' + nom : null} />
         </div>
+        <div>{children}</div>
         <div>
-        {children}
-
+          <Footer />
         </div>
-       <div>
-        <Footer />
-
-       </div>
       </body>
     </html>
   );
-
-
-
 }
+
 export default RootLayout;
