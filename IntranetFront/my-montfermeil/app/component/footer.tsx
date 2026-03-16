970e213d90ddb5547fd493de8f5a9611069c12b3
Dev montfermeil <ville.montfermeil@gmail.com>
2026-03-16 17:56:26 +0100

    Little changing

--- IntranetFront/my-montfermeil/app/component/footer.tsx
+++ IntranetFront/my-montfermeil/app/component/footer.tsx
@@ -5,11 +5,13 @@ import './stylheader.css';
 function footer() {
 
     const hauteurFooter = "50px"
+    const date = new Date()
 
+    const atoday = date.getFullYear()
     return (
         <footer className="site-footer" style={{marginTop: hauteurFooter, position: "relative"}} >
             <div className="footer-content">
-                <p>© 2025 site intranet de Montfermeil. Réservés au agents de la mairire.</p>
+                <p>© {atoday} site intranet de Montfermeil. Réservés au agents de la mairire.</p>
                 <div className="footer-links">
                     <a href="/Apropos">À propos</a>
                     <a href="mailto:service.communication@montfermeil.onmicrosoft.com">Contact</a>
